33207b17fbc594c62391261dff0ad32fec7b979a
Svaishnavi2001 <svaishnavi823@gmail.com>
2023-12-07 22:36:43 +0530

    Practiced Pattern programs and got to know about the concepts in depth.

---
+++ Pattern-Programing/src/com/Class4/Pat20.java
@@ -0,0 +1,21 @@
+package com.Class4;
+/*
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+ */
+public class Pat20 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=5-i+1;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
