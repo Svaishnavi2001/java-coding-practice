4594b528be804c139c4e965e8e55941d8148baa7
Svaishnavi2001 <svaishnavi823@gmail.com>
2023-12-09 23:51:30 +0530

    Updation

---
+++ Arrays/src/com/Class1/Lab3.java
@@ -0,0 +1,17 @@
+package com.Class1;
+import java.util.Scanner;
+public class Lab3 {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner scan=new Scanner(System.in);
+       for(int i=0;i<=4;i++){
+           System.out.println("Enter the age of "+(i+1)+" Student");
+           a[i]=scan.nextInt();
+         }
+        System.out.println("The ages are:");
+        for(int j=0;j<=4;j++){
+            System.out.println(a[j]+" ");
+
+       }
+    }
+}
---
+++ Arrays/src/com/Class1/Lab5.java
@@ -0,0 +1,17 @@
+package com.Class1;
+import java.util.Scanner;
+public class Lab5 {
+    public static void main(String[] args) {
+        float a[]=new float[5];
+        Scanner scan=new Scanner(System.in);
+
+        for(int i=0;i<=4;i++){
+            System.out.println("Enter the height");
+            a[i]=scan.nextFloat();
+        }
+        System.out.println("The heights are:");
+        for(int j=0;j<=4;j++){
+            System.out.println(a[j]+" ");
+        }
+    }
+}
