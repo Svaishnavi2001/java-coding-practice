b8f8162af99aacf49c48551e0e2adfa605213693
Svaishnavi2001 <svaishnavi823@gmail.com>
2023-12-12 00:38:33 +0530

    Practiced array concept programs.

--- Programming-Class/src/com/Arrays/Lab17a.java
+++ Programming-Class/src/com/Arrays/Lab17a.java
@@ -2,18 +2,4 @@ package com.Arrays;
 import java.util.Scanner;
 // Product of elements of an array
 public class Lab17a {
-    public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        int size=scan.nextInt();
-        int []ar=new int[size];
-
-        for(int i=0;i<ar.length;i++){
-            ar[i]=scan.nextInt();
-        }
-        int sum=1;
-        for(int i=0;i<ar.length;i++){
-            sum=sum*ar[i];
-        }
-        System.out.println(sum);
-    }
 }
