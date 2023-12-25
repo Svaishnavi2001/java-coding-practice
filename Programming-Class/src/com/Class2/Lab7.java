0439fd6db31f00e9f1a0f9930a27167ad2682f1e
Svaishnavi2001 <svaishnavi823@gmail.com>
2023-12-25 17:16:23 +0530

    Rewritten some programs

--- Programming-Class/src/com/Class2/Lab7.java
+++ Programming-Class/src/com/Class2/Lab7.java
@@ -1,30 +1,28 @@
 package com.Class2;
 import java.util.Scanner;
-// Print 1st n prime numbers.
+// Print prime numbers from 1 to n.
 public class Lab7 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
-        printPrimeNums(n);
+        printPrimeNum(n);
     }
-    private static void printPrimeNums(int n) {
-        int count=0;
-        for(int i=1;count<n;i++){
-            if(isPrime(i)==true){
-                System.out.print(i+" ");
-                count++;
-            }
-        }
+    private static void printPrimeNum(int n) {
+    for(int i=1;i<=n;i++){
+    if(isPrime(i)==true){
+        System.out.print(i+ " ");
+      }
+     }
     }
     private static boolean isPrime(int n) {
-           if(n==1){
-           return false;
+        if(n==1){
+            return false;
         }
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
+        for(int i=2;i<=n-1;i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
-        return true;
-    }
+            return true;
+        }
 }
