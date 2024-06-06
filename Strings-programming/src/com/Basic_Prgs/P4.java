4e2e146ef6d4bde2a5a074fc3b3f11131d95a12e
Svaishnavi2001 <svaishnavi823@gmail.com>
2024-06-06 23:44:03 +0530

    practiced strings programmes

---
+++ Strings-programming/src/com/Basic_Prgs/P4.java
@@ -0,0 +1,20 @@
+package com.Basic_Prgs;
+// lowercase to uppercase
+public class P4 {
+    public static void main(String[] args) {
+
+          String str = "how";
+//        String res = "";
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            char ch = str.charAt(i);
+//            if (ch>='a' && ch<='z') {
+//                res = res + (char)(ch-32);
+//            }
+//        }
+//            System.out.println(res);
+
+        System.out.println(str.toUpperCase());
+
+    }
+}
