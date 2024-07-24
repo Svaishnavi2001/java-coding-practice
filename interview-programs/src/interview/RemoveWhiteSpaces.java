ac3e87b562567f24c9c0ea4861f564966fc5dcaa
1rn19 <svaishnavi823@gmail.com>
2024-07-24 13:56:01 +0530

    Practiced DSA programs

--- interview-programs/src/interview/RemoveWhiteSpaces.java
+++ interview-programs/src/interview/RemoveWhiteSpaces.java
@@ -8,15 +8,9 @@ public class RemoveWhiteSpaces {
         s=s.replaceAll("\\s","");
         System.out.println(s);
 
-        /*char[] ch=s.toCharArray();
-        for(char c:ch){
-            System.out.println(c);
-        }*/
-        System.out.println();
-
         String[] strings = s.split(" ");
         for (String str : strings) {
-            System.out.println(str);
+            System.out.print(str);
         }
     }
 }
