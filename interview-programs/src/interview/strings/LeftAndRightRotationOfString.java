dad2a796c4e12d6e3daf758b00d91fb131190cfc
1rn19 <svaishnavi823@gmail.com>
2025-01-27 21:20:12 +0530

    Practiced some of the Array and string programs asked in interviews

---
+++ interview-programs/src/interview/strings/LeftAndRightRotationOfString.java
@@ -0,0 +1,30 @@
+package interview.strings;
+
+public class LeftAndRightRotationOfString {
+    public static void main(String[] args) {
+        String s = "abcdef";
+        int positions=2;
+        System.out.println(s);
+
+        String leftRotated=rotateLeft(s,positions);
+        System.out.println(leftRotated);
+
+        String rightRotated=rotateRight(s,positions);
+        System.out.println(rightRotated);
+    }
+    private static String  rotateLeft(String s, int positions){
+
+        int n=s.length();
+        positions=positions%n;
+
+        return s.substring(positions)+s.substring(0,positions);
+
+    }
+    private static String  rotateRight(String s, int positions){
+
+        int n=s.length();
+        positions=positions%n;
+
+        return s.substring(n-positions)+s.substring(0,n-positions);
+    }
+}
